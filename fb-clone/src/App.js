d7731a84c78368b38719cab2f1a6dc1aa8ab63f6
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-03-05 21:33:49 -0800

    added post section and styled other components

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -11,7 +11,6 @@ function App() {
       <div className="app_body">
         <Sidebar />
         <Feed />
-        {/* Feed */}
         {/* Widgets */}
       </div>
     </div>
--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import "./Feed.css";
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
+import Post from "./Post";
+import Brett from "./image0.jpeg";
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
-            {/* MessageSender */}
+            <MessageSender />
+            <Post
+                profilePic={Brett}
+                message='Wow this is cool'
+                timestamp="this is a timestamp"
+                username="Bwinterstrom"
+                image="https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=1390&crop=1"
+            />
+            <Post
+                profilePic={Brett}
+                message='Wow this is cool'
+                timestamp="this is a timestamp"
+                username="Bwinterstrom"
+            />
+            <Post />
         </div>
     )
 }
