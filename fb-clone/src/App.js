11dd6e310fd01711b637f4f042eaf0dd3c6c8b8e
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-03-05 18:36:48 -0800

    added navbar and styling for navbar

---
+++ fb-clone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <div className="app_body">
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBi-zMmseFsa1FFIbej-q093J7--8tXgoQ",
+    authDomain: "fb-clone-c4a27.firebaseapp.com",
+    projectId: "fb-clone-c4a27",
+    storageBucket: "fb-clone-c4a27.appspot.com",
+    messagingSenderId: "446351881544",
+    appId: "1:446351881544:web:8a2ad4eee0f8780031ef7d"
+};
